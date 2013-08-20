56df5b5e2b0521493edcd437da2ea3bafb732d29
rikin311 <riks311@gmail.com>
2013-08-19 23:53:53 -0400

    Initial Commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Itsjc::Application.config.secret_token = 'e5c54d5cb60769800f7e3dbbb09caf31fd9179ba53fd0fbfa352745a7cd2b257b06d0396fbe81d8a1c87cd38e51653297d0c630d33883cc847c64cd0fc8c7d4e'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,8 @@
+# Be sure to restart your server when you modify this file.
+
+Itsjc::Application.config.session_store :cookie_store, key: '_itsjc_session'
+
+# Use the database for sessions instead of the cookie-based default,
+# which shouldn't be used to store highly confidential information
+# (create the session table with "rails generate session_migration")
+# Itsjc::Application.config.session_store :active_record_store
